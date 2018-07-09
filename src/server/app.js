b942cbba7b07a0af4c5d49e6b8f139866c48b32c
phchu <phchu@imedlar.com>
2018-07-09 11:32:21 +0800

    refactor: modify import by ES6 & remove useless expressions

--- src/server/app.js
+++ src/server/app.js
@@ -1,54 +1,51 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const validator = require('express-validator');
-const errorHandler = require('errorhandler');
-const api = require('./api/index');
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import errorHandler from 'errorhandler';
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+
+import api from './api/index';
+
 const app = express();
 
 const PORT = process.env.NODE_ENV !== 'development' ? 3000 : 8080;
-const server = app.listen(PORT, () => console.log(`Listening on PORT ${PORT}!`));
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}!`));
 
 app.use(bodyParser.json({
-    limit: '10mb'
+  limit: '10mb'
 }));
 app.use(bodyParser.urlencoded({
-    limit: '10mb',
-    extended: true
+  limit: '10mb',
+  extended: true
 }));
 
 app.use(express.static('dist'));
 app.use(logger('dev'));
-app.use(validator()); // this line must be immediately after express.bodyParser()!
 
 app.use(cookieParser());
 app.use((req, res, next) => {
-    // req.user = {_id: '5840e3481ee48c1a3d68aaaa'}; //temp: set dev admin user._id
-    res.locals.login = req.isAuthenticated();
-    res.locals.user = req.user;
-    next();
+  // req.user = {_id: '5840e3481ee48c1a3d68aaaa'}; //temp: set dev admin user._id
+  res.locals.login = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
 });
 
 app.use('/api', api);
 
 if (process.env.NODE_ENV !== 'development') {
-    app.get('*', (req, res) => {
-        let filePath = './dist/index.html'
-        let index = path.resolve(filePath);
-        console.log(index);
-        res.sendFile(index)
-    });
+  app.get('*', (req, res) => {
+    const filePath = './dist/index.html';
+    const index = path.resolve(filePath);
+    res.sendFile(index);
+  });
 }
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found!');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found!');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -56,7 +53,7 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(errorHandler());
+  app.use(errorHandler());
 }
 
 module.exports = app;
