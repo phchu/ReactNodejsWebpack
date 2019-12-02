507a7c22eca56cefcbf97b165bb537a0410c193e
phchu <penghua.chu@gmail.com>
2019-12-02 15:19:17 +0800

    chore: refine splitChunks

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,11 +4,15 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WebpackAutoInject = require('webpack-auto-inject-version');
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const webpackBundleAnalyzer = require('webpack-bundle-analyzer');
+const nodeObjectHash = require('node-object-hash');
 const {
   CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 
+const smp = new SpeedMeasurePlugin();
 const {
   BundleAnalyzerPlugin
 } = webpackBundleAnalyzer;
@@ -16,7 +20,7 @@ const DIST = path.join(__dirname, '..', 'dist', 'public');
 const dateFormat = 'mmddhhMM';
 module.exports = (env, argv) => {
   const isDEV = argv.mode === 'development';
-  return ({
+  return (smp.wrap({
     name: 'client',
     entry: './index.js',
     output: {
@@ -101,15 +105,62 @@ module.exports = (env, argv) => {
       }
     },
     optimization: {
+      runtimeChunk: 'single',
       splitChunks: {
-        chunks: 'all'
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          'graphql-vendor': {
+            test: /[\\/]node_modules[\\/](apollo-cache-inmemory|apollo-client|apollo-link|apollo-link-error|apollo-link-http|graphql|graphql-tag)[\\/]/
+          },
+          'react-vendor': {
+            test: /[\\/]node_modules[\\/](react|react-dom|history|prop-types|react-apollo|react-apollo-hooks|react-fa|react-loadable|react-router-dom)[\\/]/
+          },
+          utility: {
+            test: /[\\/]node_modules[\\/](lodash|moment|moment-timezone)[\\/]/
+          },
+          'antd-vendor': {
+            test: (module) => (/antd/.test(module.context)),
+            priority: 2,
+            reuseExistingChunk: false
+          }
+        }
       },
-      runtimeChunk: true,
       minimize: true,
       minimizer: [
-        new TerserPlugin({
-          sourceMap: true,
+        new HardSourceWebpackPlugin({
+          configHash: (webpackConfig) => nodeObjectHash({ sort: false }).hash(webpackConfig),
+          environmentHash: {
+            root: process.cwd(),
+            directories: [],
+            files: ['package-lock.json', 'yarn.lock'],
+          }
         }),
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+          parallel: true,
+          cache: true,
+          sourceMap: false,
+        })
       ],
     },
     plugins: [
@@ -120,6 +171,7 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: '../public/index.html',
         favicon: '../public/favicon.ico',
+        inject: true
       }),
       new ExtractTextPlugin({
         filename: 'assets/css/[hash:8]-[name].css',
@@ -146,5 +198,5 @@ module.exports = (env, argv) => {
       new CompressionPlugin(),
       new BundleAnalyzerPlugin()
     ]
-  });
+  }));
 };
